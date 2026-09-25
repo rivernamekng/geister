8ac5306993cbf7e6e1afb2cd83261a3790682838
rivernamekng <rivernamekng@gmail.com>
2026-09-25 15:22:28 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,22 @@
-/* Firebase の設定。オンライン対戦を使うときだけ必要です。
-   Firebase コンソールで作った Web アプリの設定をここに貼り付けてください。
-   手順は README.md にあります。貼り替えるまでオンライン対戦のボタンは無効のままです。
-   （CPU戦と1台での対戦は、この設定なしでも遊べます） */
-window.GeisterFirebaseConfig = {
-  apiKey: 'ここにapiKeyを貼る',
-  authDomain: 'ここにauthDomainを貼る',
-  databaseURL: 'ここにdatabaseURLを貼る',
-  projectId: 'ここにprojectIdを貼る',
-  appId: 'ここにappIdを貼る'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRCQi0IoucY8jwgDRDogKnr8_lpGtOWKY",
+  authDomain: "geister-6d6cf.firebaseapp.com",
+  databaseURL: "https://geister-6d6cf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "geister-6d6cf",
+  storageBucket: "geister-6d6cf.firebasestorage.app",
+  messagingSenderId: "677190632266",
+  appId: "1:677190632266:web:56f87540f96d82c8f7f7b3",
+  measurementId: "G-PSR8QJ4HTG"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
