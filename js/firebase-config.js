f25b9fef878f1d25088e3b172f9f5ad8f06724e1
rivernamekng <rivernamekng@gmail.com>
2026-09-26 00:37:36 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,3 @@
-/* Firebase の設定。オンライン対戦を使うときだけ必要です。
-   Firebase コンソールで作った Web アプリの設定をここに貼り付けてください。
-   手順は README.md にあります。貼り替えるまでオンライン対戦のボタンは無効のままです。
-   （CPU戦と1台での対戦は、この設定なしでも遊べます） */
 window.GeisterFirebaseConfig = {
   apiKey: 'AIzaSyChDDlGIusCtYRJwQRDepKp8_Lg0OMxY',
   authDomain: 'geister-6d6cf.firebaseapp.com',
